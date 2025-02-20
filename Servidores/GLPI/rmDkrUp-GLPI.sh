041ef3c806a3bf798ebc480bcdca090e65db84fe
Ricardo MONLA <rmonla@gmail.com>
2025-02-20 18:43:43 -0300

    rmDkrUp-GLPI.sh  - v250220-1841

---
+++ Servidores/GLPI/rmDkrUp-GLPI.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+# Script para configurar y desplegar GLPI en Docker
+# Ricardo MONLA (https://github.com/rmonla)
+# rmDkrUp-GLPI.sh  - v250220-1841
+
+# Variables del Docker
+dkrVRS=$(cat <<YAML
+
+dkrNOM=homer
+dkrPOR=8080
+
+dkrArchENV=.env
+dkrArchYML=docker-compose.yml
+
+appDirCFG=config
+YAML
+)
+
+dkrYML=$(cat <<YAML
+services:
+  homer:
+    image: b4bz/homer
+    container_name: \${dkrNOM}
+    volumes:
+      - ./\${appDirCFG}:/www/assets # Make sure your local config directory exists
+    ports:
+      - \${dkrPOR}:8080
+    user: 1000:1000 # default
+    environment:
+      - INIT_ASSETS=1 # default, requires the config directory to be writable for the container user (see user option)
+    restart: unless-stopped
+YAML
+)
+# ---
+
+# Procesar el contenido de dkrVRS y exportar las variables
+eval "$(echo "$dkrVRS" | grep -E '^[a-zA-Z_][a-zA-Z0-9_]*=' | sed 's/^/export /')"
+# ---
+
+crear_directorio() {
+    for newDir in "$@"; do
+        echo "Creando el directorio $newDir"
+        mkdir -p "$newDir" || { echo "Error al crear el directorio $newDir"; exit 1; }
+    done
+}
+
+dirDKR="$(pwd)/$dkrNOM"
+
+directorios=(
+    "$dirDKR/$appDirCFG"
+    # "$dirDKR"
+)
+
+crear_directorio "${directorios[@]}"
+# ---
+
+escribir_archivo() {
+    echo "Creando el archivo $2"
+    echo "$1" > "$2" || { echo "Error al escribir $2"; exit 1; }
+}
+escribir_archivo "${dkrVRS}" "$dirDKR/$dkrArchENV" # Variables de entorno de Docker
+escribir_archivo "${dkrYML}" "$dirDKR/$dkrArchYML" # Archivo de despliegue de Docker
+# ---
+
+# Ejecutar docker-compose
+archDkrComp="$dirDKR/$dkrArchYML"
+echo "Iniciando el contenedor con docker-compose..."
+docker compose -f "$archDkrComp" up -d || { echo "Error al ejecutar docker-compose"; exit 1; }
+
+# Mensaje de finalización
+echo "${dkrNOM} se ha desplegado correctamente en http://0.0.0.0:${dkrPOR}/"
